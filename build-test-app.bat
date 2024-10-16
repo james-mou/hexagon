951150658bd26650f2a16e21c0026f96af27adf8
James Mou <jamesmoud0dd@gmail.com>
2024-10-16 19:21:51 +0000

    forgot to delete something

--- build-test-app.bat
+++ build-test-app.bat
@@ -1,13 +1,3 @@
-#!/bin/sh
-#navigate to the app directory
-cd app
-#install dependencies
-npm install
-#build it
-npm run build
-#run tests
-npm run test
-
 @echo off
 REM navigate to the app directory
 cd app
