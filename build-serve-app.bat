d20e27f01e51c06e2edfe8f9472521f83457c9b7
James Mou <jamesmoud0dd@gmail.com>
2024-10-16 18:48:43 +0000

    forgot to delete something

--- build-serve-app.bat
+++ build-serve-app.bat
@@ -1,12 +1,3 @@
-#!/bin/sh
-# navigate to the app directory
-cd app
-# install all dependencies
-npm install
-# build the app
-npm run build
-# run it
-npm run dev
 
 @echo off
 REM navigate to the app directory
